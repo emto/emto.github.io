4cff334e85807c8da18b2c05bbe3c158c34d611f
Emily To <emilytoe@gmail.com>
2015-08-26 06:20:28 -0700

    snap progress

--- js/controllers/AboutCtrl.js
+++
@@ -1,8 +0,0 @@
-personal.controller('AboutController', function($scope, $location) {
-    $scope.pageClass = 'page-about';
-
-    $scope.place = function(url) {
-        $location.path("/" + url);
-    }
-
-});
--- js/controllers/MainCtrl.js
+++
@@ -1,6 +0,0 @@
-personal.controller('MainController', function($scope, $location) {
-    $scope.place = function(url) {
-        $location.path("/" + url);
-    }
-    $scope.pageClass = 'page-home';
-});
